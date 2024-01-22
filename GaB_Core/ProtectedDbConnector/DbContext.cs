ba5f25d255285872b6766c3e69a83e60a7e00133
Николай Перминов <kolya-perminov@ya.ru>
2024-01-22 23:16:41 +0500

    Добавьте файлы проекта.

---
+++ GaB_Core/ProtectedDbConnector/DbContext.cs
@@ -0,0 +1,16 @@
+﻿using GaB_Core.ProtectedDbConnector.Models;
+using Microsoft.EntityFrameworkCore;
+namespace GaB_Core.ProtectedDbConnector;
+public class ApplicationContext : DbContext
+{
+    public DbSet<User> Users { get; set; } = null!;
+
+    public ApplicationContext()
+    {
+        Database.EnsureCreated();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql(GAB_Core.ConfigurationManager.ConnectionStringProtectedDb);
+    }
+}
